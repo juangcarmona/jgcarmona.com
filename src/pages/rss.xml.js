8aea277948939558c7db65f3db22144402eee7ee
Juan G. Carmona <juan.garciacarmona@gmail.com>
2026-01-29 08:30:29 +0100

    Improved RSS.xml generation

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,15 +2,37 @@ import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
+const MAX_ITEMS = 50;
+
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/${post.id}/`,
-		})),
-	});
+  const posts = await getCollection('blog');
+
+  const sorted = posts
+    .sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate))
+    .slice(0, MAX_ITEMS);
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+
+    xmlns: {
+      atom: 'http://www.w3.org/2005/Atom',
+    },
+
+    customData: `
+      <atom:link
+        href="${context.url.href}"
+        rel="self"
+        type="application/rss+xml"
+      />
+    `,
+
+    items: sorted.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/${post.id}/`,
+    })),
+  });
 }
